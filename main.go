4ff0b45cd9824224397f1e5893b3e3d1d9f5c56a
Meghan <nektromc@gmail.com>
2019-06-03 00:03:27 -0400

    add AllowAll() which disables the filter completely

--- main.go
+++ main.go
@@ -9,13 +9,21 @@ import (
 
 var (
 	allowed = []string{}
+	filter  = true
 )
 
 func Allow(s string) {
 	allowed = append(allowed, s)
 }
 
+func AllowAll() {
+	filter = false
+}
+
 func Allowed(r *http.Request) bool {
+	if !filter {
+		return true
+	}
 	u, _ := url.Parse("http://" + r.Host)
 	return util.Contains(allowed, u.Hostname())
 }
